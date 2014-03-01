aad0d8869d2d0be0d28ae1c88e28df1352d79d62
Joel Marcinik <jdm329@drexel.edu>
2014-03-01 13:25:17 -0500

    added skeleton for topic match class, feel free to change

---
+++ src/main/java/cs492/obsecurefinal/algorithm/TopicMatcher.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs492.obsecurefinal.algorithm;
+
+import cc.mallet.types.InstanceList;
+
+/**
+ *
+ * @author JOEL
+ */
+public class TopicMatcher 
+{
+    public TopicMatcher(InstanceList a, InstanceList b)
+    {
+        
+    }
+    
+    public double getMatchValue()
+    {
+        
+    }
+}
