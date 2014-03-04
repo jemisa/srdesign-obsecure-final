789691daf5a667c8e2ef0b941d94c7f9755060b0
Joel Marcinik <jdm329@drexel.edu>
2014-03-04 18:37:35 -0500

    Removed unused imports

--- src/main/java/cs492/obsecurefinal/algorithm/TopicMatcher.java
+++ src/main/java/cs492/obsecurefinal/algorithm/TopicMatcher.java
@@ -4,11 +4,9 @@
  */
 package cs492.obsecurefinal.algorithm;
 
-import cc.mallet.types.InstanceList;
 import cs492.obsecurefinal.common.Topic;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  *
@@ -34,7 +32,8 @@ public class TopicMatcher
         {
             ArrayList<Integer> uniqueTopics = new ArrayList<>();        
             double countCloseMatch = 0.0; 
-
+            
+            // Sort to have a greater probability of matching right away.
             Arrays.sort(topicListA);
             Arrays.sort(topicListB);
 
@@ -42,6 +41,7 @@ public class TopicMatcher
             {                     
                 for(int j = 0; j < topicListB.length; j++)
                 {                 
+                    // Track the number of unique topic id's uncovered
                     if(!uniqueTopics.contains(topicListA[i].getId()))
                         uniqueTopics.add(topicListA[i].getId());
 
@@ -50,6 +50,8 @@ public class TopicMatcher
 
                     if(topicListA[i].getId() == topicListB[j].getId())
                     {
+                        // For matching topics, see if the two probabilities are close.
+                        
                         double probA = topicListA[i].getProbability();
                         double probB = topicListB[j].getProbability();
 
@@ -57,7 +59,7 @@ public class TopicMatcher
                         double fractionB = THRESHOLD_MULTIPLIER * probB;
 
                         if(probA > probB - fractionB && probA < probB + fractionB)
-                            countCloseMatch += 1.0 * ((probA + probB) / 2); // use average prob to weight it
+                            countCloseMatch += 1.0 * ((probA + probB) / 2.0); // use average prob to weight it
                             //countCloseMatch++; // unweighted version
                     }
                 }
