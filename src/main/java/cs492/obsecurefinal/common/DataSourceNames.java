6b056d4300e1162e5f430fd8c52d9fb6803da978
Ryan Hersh <supersmasher100@yahoo.com>
2014-03-04 18:53:28 -0500

    Names

---
+++ src/main/java/cs492/obsecurefinal/common/DataSourceNames.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs492.obsecurefinal.common;
+
+/**
+ *
+ * @author Ryan Hersh
+ */
+public class DataSourceNames 
+{
+    static final String locExtStrat = "models/opennlp/ner/en-ner-location.bin";
+    static final String workExtStrat = "models/opennlp/ner/en-ner-location.bin";
+    static final String topicIdent = "stoplist.txt";
+    static final String santSimple = "en-sent.bin";    
+}
