d86458744bccf70c4289fe76efd2caf6d27cc598
Joel Marcinik <jdm329@drexel.edu>
2014-03-04 18:34:09 -0500

    Added matching capability

--- src/main/java/cs492/obsecurefinal/algorithm/TopicMatcher.java
+++ src/main/java/cs492/obsecurefinal/algorithm/TopicMatcher.java
@@ -6,21 +6,66 @@ package cs492.obsecurefinal.algorithm;
 
 import cc.mallet.types.InstanceList;
 import cs492.obsecurefinal.common.Topic;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  *
- * @author JOEL
+ * @author Joel Marcinik
+ * Compares two topic lists to determine if they refer to the same context
  */
 public class TopicMatcher 
 {
+    public static final double THRESHOLD_MULTIPLIER = 0.1;
+    
+    private  Topic[] topicListA, topicListB;
+    
     public TopicMatcher(Topic[] a, Topic[] b)
     {
-        
+        topicListA = a.clone();
+        topicListB = b.clone();       
     }
     
+    // Return how close the match is.
     public double getMatchValue()
     {
-        double value = 0;
-        return value;
+        if(topicListA.length > 0 && topicListB.length > 0)
+        {
+            ArrayList<Integer> uniqueTopics = new ArrayList<>();        
+            double countCloseMatch = 0.0; 
+
+            Arrays.sort(topicListA);
+            Arrays.sort(topicListB);
+
+            for(int i = 0; i < topicListA.length; i++)
+            {                     
+                for(int j = 0; j < topicListB.length; j++)
+                {                 
+                    if(!uniqueTopics.contains(topicListA[i].getId()))
+                        uniqueTopics.add(topicListA[i].getId());
+
+                    if(!uniqueTopics.contains(topicListB[j].getId()))
+                        uniqueTopics.add(topicListB[j].getId());
+
+                    if(topicListA[i].getId() == topicListB[j].getId())
+                    {
+                        double probA = topicListA[i].getProbability();
+                        double probB = topicListB[j].getProbability();
+
+                        double fractionA = THRESHOLD_MULTIPLIER * probA;
+                        double fractionB = THRESHOLD_MULTIPLIER * probB;
+
+                        if(probA > probB - fractionB && probA < probB + fractionB)
+                            countCloseMatch += 1.0 * ((probA + probB) / 2); // use average prob to weight it
+                            //countCloseMatch++; // unweighted version
+                    }
+                }
+            }
+
+            return countCloseMatch/(double)(uniqueTopics.size());
+        }
+        else
+            return 0.0;
     }
 }
