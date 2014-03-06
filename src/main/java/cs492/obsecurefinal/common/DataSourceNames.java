88799fcbde454baad57aa98707e6beb1df2230dd
Joel Marcinik <jdm329@drexel.edu>
2014-03-06 13:41:05 -0500

    Adjusted constant name and replaced direct file reference in TI

--- src/main/java/cs492/obsecurefinal/common/DataSourceNames.java
+++ src/main/java/cs492/obsecurefinal/common/DataSourceNames.java
@@ -14,7 +14,7 @@ public class DataSourceNames
 {
     public static final String LOC_MODEL_FILE = "models/opennlp/ner/en-ner-location.bin";
     public static final String WORKPLACE_MODEL_FILE = "models/opennlp/ner/en-ner-location.bin";
-    public static final String TOPICS_SKIP_FILE = "stoplist.txt";
+    public static final String TOPICS_STOPWORDS = "stoplist.txt";
     public static final String SENT_MODEL_FILE = "en-sent.bin";
     public static final String DB_NAME = "obsecure";
     public static final String DB_URL = "jdbc:derby://localhost:1527/obsecure-final";
