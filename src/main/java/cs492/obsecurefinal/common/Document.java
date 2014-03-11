6206d02575beb168506ab2a2012c59b014f41d86
Joel Marcinik <jdm329@drexel.edu>
2014-03-11 18:06:09 -0400

    Fixed indents, added missing status values

--- src/main/java/cs492/obsecurefinal/common/Document.java
+++ src/main/java/cs492/obsecurefinal/common/Document.java
@@ -17,146 +17,147 @@ import opennlp.tools.sentdetect.SentenceModel;
  */
 public class Document
 {
-        private String text;
-        String filename;
-        Sentence[] sentences;
-        private boolean valid, sentencesCached;
-                
-        public Document(String text)
+    private String text;
+    String filename;
+    Sentence[] sentences;
+    private boolean valid, sentencesCached;
+
+    public Document(String text)
+    {
+        this.text = text;
+
+        valid = true;
+        sentencesCached = false;
+
+        //valid = splitDocument();
+    }
+
+    public Document(File file)
+    {
+        if(file.exists())
         {
-            this.text = text;
-            
-            valid = true;
-            sentencesCached = false;
-            
-            //valid = splitDocument();
+            filename = file.getAbsolutePath();
+
+            text = "";
+
+            try
+            {
+                FileReader fReader = new FileReader(file);
+
+                int c = fReader.read();
+                while(c != -1)
+                {
+                    text += (char)c;
+                }
+
+                fReader.close();
+
+                //valid = splitDocument();
+                valid = true;
+                sentencesCached = false;
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace(System.out);
+                valid = false;
+                sentencesCached = false;
+            }
+        }
+    }
+
+    // returns true if succesful, false otherwise
+    private boolean splitDocument()
+    {
+        //FileInputStream modelInput;
+        InputStream modelInput;
+        SentenceModel sm;
+
+        try
+        { 
+            //modelInput  = new FileInputStream(DataSourceNames.SENT_MODEL_FILE);
+            modelInput = Document.class.getResourceAsStream(DataSourceNames.SENT_MODEL_FILE);
+
+            sm = new SentenceModel(modelInput);
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace(System.out);
+            return false;
         }
 
-        public Document(File file)
+        try
         {
-            if(file.exists())
+            if(modelInput != null)            
+                modelInput.close();
+        }
+        catch(Exception ex)
+        {                
+            ex.printStackTrace(System.out);
+            return false;
+        }
+
+        if(sm != null)
+        {
+            SentenceDetectorME detector = new SentenceDetectorME(sm);
+
+            String[] sentenceStrings = detector.sentDetect(text);                
+            sentences = Sentence.convertStringArray(sentenceStrings);
+
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+    public void setText(String newText)
+    {
+        this.text = newText;
+        valid = true;
+        sentencesCached = false;
+    }
+
+    public boolean isValid()
+    {
+        return valid;
+    }
+
+    public void saveToFile(File f)
+    {
+
+    }
+
+    public String getText()
+    {
+        if(valid)
+            return text;
+        else
+            return "";
+    }
+        
+    public Sentence[] getSentences()
+    {
+        if(valid)
+        {
+            if(sentencesCached)
+                return sentences;
+            else
             {
-                filename = file.getAbsolutePath();
-                
-                text = "";
-                
-                try
+                if(splitDocument())
                 {
-                    FileReader fReader = new FileReader(file);
-                    
-                    int c = fReader.read();
-                    while(c != -1)
-                    {
-                        text += (char)c;
-                    }
-                    
-                    fReader.close();
-                    
-                    //valid = splitDocument();
+                    sentencesCached = true;
                     valid = true;
-                    sentencesCached = false;
+                    return sentences;                    
                 }
-                catch(Exception ex)
-                {
-                    ex.printStackTrace(System.out);
-                    valid = false;
-                    sentencesCached = false;
-                }
-            }
-        }
-        
-        // returns true if succesful, false otherwise
-        private boolean splitDocument()
-        {
-            //FileInputStream modelInput;
-            InputStream modelInput;
-            SentenceModel sm;
-
-            try
-            { 
-                //modelInput  = new FileInputStream(DataSourceNames.SENT_MODEL_FILE);
-                modelInput = Document.class.getResourceAsStream(DataSourceNames.SENT_MODEL_FILE);
-                
-                sm = new SentenceModel(modelInput);
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace(System.out);
-                return false;
-            }
-            
-            try
-            {
-                if(modelInput != null)            
-                    modelInput.close();
-            }
-            catch(Exception ex)
-            {                
-                ex.printStackTrace(System.out);
-                return false;
-            }
-            
-            if(sm != null)
-            {
-                SentenceDetectorME detector = new SentenceDetectorME(sm);
-
-                String[] sentenceStrings = detector.sentDetect(text);                
-                sentences = Sentence.convertStringArray(sentenceStrings);
-                
-                return true;
-            }
-            else
-                return false;
-            
-        }
-        
-        public void setText(String newText)
-        {
-            this.text = newText;
-            valid = true;
-            sentencesCached = false;
-        }
-
-        public boolean isValid()
-        {
-            return valid;
-        }
-        
-        public void saveToFile(File f)
-        {
-        
-        }
-
-        public String getText()
-        {
-            if(valid)
-                return text;
-            else
-                return "";
-        }
-        
-        public Sentence[] getSentences()
-        {
-            if(valid)
-            {
-                if(sentencesCached)
-                    return sentences;
                 else
                 {
-                    if(splitDocument())
-                    {
-                        sentencesCached = true;
-                        return sentences;
-                    }
-                    else
-                        return new Sentence[] {};
+                    valid = false;
+                    sentencesCached = false;
+                    return new Sentence[] {};                
                 }
             }
-            else
-                return new Sentence[] {};
         }
-        
-        //public TokenSplitDocument createTokenSplitDocument()
-        //{              
-        //}
+        else
+            return new Sentence[] {};
+    }
 }
