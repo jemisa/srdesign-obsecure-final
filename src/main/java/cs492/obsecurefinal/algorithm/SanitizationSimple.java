172167318dcac999b0415b9d5c3e75cdb8cbd5a7
Joel Marcinik <jdm329@drexel.edu>
2014-04-01 09:45:51 -0400

    added ngram checking to sanitization algorithm

--- src/main/java/cs492/obsecurefinal/algorithm/SanitizationSimple.java
+++ src/main/java/cs492/obsecurefinal/algorithm/SanitizationSimple.java
@@ -6,6 +6,7 @@ package cs492.obsecurefinal.algorithm;
 
 import cc.mallet.topics.ParallelTopicModel;
 import cs492.obsecurefinal.builder.InferenceBuilder;
+import cs492.obsecurefinal.builder.NGramBuilder;
 import cs492.obsecurefinal.builder.TopicBuilder;
 import cs492.obsecurefinal.cluster.BrownClusters;
 import cs492.obsecurefinal.common.Agent;
@@ -34,7 +35,9 @@ import java.util.Map;
  */
 public class SanitizationSimple extends Sanitization
 {
-    public static final double EQUALITY_THRESHOLD = 0.1;
+    public static final double EQUALITY_THRESHOLD = 0.5;
+    public static final int MAX_WINDOW = 3;
+    public static final int MIN_WINDOW = 2;
     
     Document doc;
     Agent profile;
@@ -66,6 +69,8 @@ public class SanitizationSimple extends Sanitization
             
             TopicIdentifier ident = new TopicIdentifier(master); //new TopicIdentifier();
             InferenceBuilder infBuilder = new InferenceBuilder(master); //new InferenceBuilder();
+                      
+            NGramBuilder ngramBuilder = new NGramBuilder();
             
             SanitizationResult finalResult = new SanitizationResult();
             
@@ -116,53 +121,82 @@ public class SanitizationSimple extends Sanitization
                     {
                         privateEntities.put(ent, Boolean.FALSE);
                         
-                        // Run inference on sentence and context
-                         Topic[] topicList =  ident.readFromStrings(new String[] {prevSentence, sentence, nextSentence});
-                                           
-                        // Remove entity from the sentence
-                        //Span entitySpan = ent.getSpan();
-                        //String s1 = sentence.substring(0, entitySpan.getStart());
-                        //String s2 = sentence.substring(entitySpan.getEnd(), sentence.length() - 1);
-                        String sentenceNoEntity = ent.getSentenceNoEntity().getText();
-                                            
-                        // Run the inference on the entity-less sentence and context
-                        Topic[] topicListNoEntities = ident.readFromStrings(new String[] {prevSentence, sentenceNoEntity, nextSentence});
-                                             
-                        List<Topic[]> profileInferences = new ArrayList<>();
-                                               
-                        // Get all private information about the agent, and topic distribution
-                        // associated with characteristic of the agent
-                        for(EntityTypes type: EntityTypes.values())
-                        {
-                            String profileEntity = profile.getCharacteristic(type).toUpperCase();
-                            Topic[] infTopics = infBuilder.loadInference(profileEntity);
-                            if(infTopics.length > 0)
-                                profileInferences.add(infTopics);
-                        }
-                        
-                        // Load topic distributions for the type of privacy data to which the entity
-                        // belongs
-                        Topic[] infPrivacyType = infBuilder.loadInference(ent.getType().toString());
-                        
-                        // For each topic inference related to the agent,
-                        // check if the sentence matches
-                        for(Topic[] inf : profileInferences)
-                        {
-                            TopicMatcher matcher = new TopicMatcher(inf, topicList);
-                            TopicMatcher matcherNoEntities = new TopicMatcher(inf, topicListNoEntities);                            
-                            TopicMatcher matcherInfoType = new TopicMatcher(inf, infPrivacyType);
-                            
-                            double matchWithEntities = matcher.getMatchValue();
-                            double matchWithNoEntities = matcherNoEntities.getMatchValue();
-                            double matchType = matcherInfoType.getMatchValue();
-                            
-                            //if (matchWithEntities > EQUALITY_THRESHOLD && matchWithNoEntities < matchWithEntities) // TODO: adjust threshold value
-                            //if(matchWithEntities > EQUALITY_THRESHOLD && matchType > EQUALITY_THRESHOLD)
-                            if(matchWithEntities > matchWithNoEntities)
+                        if(prevSentence.length() + sentence.length() + nextSentence.length() > 100)
+                        {                        
+                            // Run inference on sentence and context
+                             Topic[] topicList =  ident.readFromStrings(new String[] {prevSentence, sentence, nextSentence});
+
+                            // Remove entity from the sentence
+                            //Span entitySpan = ent.getSpan();
+                            //String s1 = sentence.substring(0, entitySpan.getStart());
+                            //String s2 = sentence.substring(entitySpan.getEnd(), sentence.length() - 1);
+                            String sentenceNoEntity = ent.getSentenceNoEntity().getText();
+
+                            // Run the inference on the entity-less sentence and context
+                            Topic[] topicListNoEntities = ident.readFromStrings(new String[] {prevSentence, sentenceNoEntity, nextSentence});
+
+                            List<Topic[]> profileInferences = new ArrayList<>();
+
+                            // Get all private information about the agent, and topic distribution
+                            // associated with characteristic of the agent
+                            for(EntityTypes type: EntityTypes.values())
                             {
-                                // mark entity as private
-                                privateEntities.put(ent, Boolean.TRUE);     
-                                break;
+                                String profileEntity = profile.getCharacteristic(type).toUpperCase();
+                                Topic[] infTopics = infBuilder.loadInference(profileEntity);
+                                if(infTopics.length > 0)
+                                    profileInferences.add(infTopics);
+                            }
+
+
+
+                            // Load topic distributions for the type of privacy data to which the entity
+                            // belongs
+                            Topic[] infPrivacyType = infBuilder.loadInference(ent.getType().toString());
+
+                            // For each topic inference related to the agent,
+                            // check if the sentence matches
+                            for(Topic[] inf : profileInferences)
+                            {
+                                TopicMatcher matcher = new TopicMatcher(inf, topicList);
+                                TopicMatcher matcherNoEntities = new TopicMatcher(inf, topicListNoEntities);                            
+                                TopicMatcher matcherInfoType = new TopicMatcher(inf, infPrivacyType);
+
+                                double matchWithEntities = matcher.getMatchValue();
+                                double matchWithNoEntities = matcherNoEntities.getMatchValue();
+                                double matchType = matcherInfoType.getMatchValue();
+
+                                //if (matchWithEntities > EQUALITY_THRESHOLD && matchWithNoEntities < matchWithEntities) // TODO: adjust threshold value
+                                //if(matchWithEntities > EQUALITY_THRESHOLD && matchType > EQUALITY_THRESHOLD)
+                                if(matchWithEntities > EQUALITY_THRESHOLD)
+                                {
+                                    // mark entity as private
+                                    privateEntities.put(ent, Boolean.TRUE);     
+                                    break;
+                                }
+                            }
+                        }
+                        else // if not enough text, use ngrams to check for privacy contents
+                        {
+                            // Get ngrams associated with each topic value
+                            List<Map<String, Integer>> storedNgrams = new ArrayList<>();
+                            for(EntityTypes type: EntityTypes.values())
+                            {
+                                Map<String, Integer> ngramset = ngramBuilder.LoadNGrams(text.toUpperCase() + "_NGRAM");
+                                storedNgrams.add(ngramset);
+                            }
+                            
+                            String text = sentence.toUpperCase();
+        
+                            for(Map<String, Integer> topicalNGram: storedNgrams)
+                            {
+                                for(String keyTerm: topicalNGram.keySet())
+                                {
+                                    if(text.contains(keyTerm.toUpperCase()))
+                                    {
+                                        privateEntities.put(ent, Boolean.TRUE);
+                                        break;
+                                    }
+                                }
                             }
                         }
                     }
@@ -189,41 +223,40 @@ public class SanitizationSimple extends Sanitization
                 }
             }
             
+            // Get all private information about the user, and topics associated
+            // with each piece of private information
+            List<Topic[]> profileInferences = new ArrayList<>();
+            for(EntityTypes type: EntityTypes.values())
+            {
+                String profileEntity = profile.getCharacteristic(type);
+                Topic[] infTopics = infBuilder.loadInference(profileEntity.toUpperCase());
+                if(infTopics.length > 0)
+                    profileInferences.add(infTopics);
+            }
+            
+            List<Map<String, Integer>> storedNgrams = new ArrayList<>();
+            for(EntityTypes type: EntityTypes.values())
+            {
+                Map<String, Integer> ngramset = ngramBuilder.LoadNGrams(text.toUpperCase() + "_NGRAM");
+                storedNgrams.add(ngramset);
+            }
+            
             // Check sentences which contained no entities, and see if they contain private information
             // that can't be manually taken out.
             for(int i = 0; i < sentences.length; i++)
             {
                 if(sentencePrivacyValue.get(sentences[i]) == PrivacyStatus.UNKNOWN)
                 {
-                    Topic[] topicList = ident.readFromStrings(new String[] {sentences[i].getText()});
-                    
-                     List<Topic[]> profileInferences = new ArrayList<>();
-                    
-                    // Get all private information about the user, and topics associated
-                    // with each piece of private information
-                    for(EntityTypes type: EntityTypes.values())
+                    if(checkSentenceNgramMatch(sentences[i], storedNgrams))
                     {
-                        String profileEntity = profile.getCharacteristic(type);
-                        Topic[] infTopics = infBuilder.loadInference(profileEntity.toUpperCase());
-                        if(infTopics.length > 0)
-                            profileInferences.add(infTopics);
+                        SanitizationHint hint = new HintNoReplacements(sentences[i], 1);
+                        finalResult.addHint(hint);
                     }
-                    
-                    // For each topic inference related to the agent,
-                    // check if the sentence matches
-                    
-                    for(Topic[] inf : profileInferences)
-                    {
-                        TopicMatcher matcher = new TopicMatcher(inf, topicList);         
-                        double match = matcher.getMatchValue();
-                        
-                        if(match > EQUALITY_THRESHOLD)
-                        {
-                            SanitizationHint hint = new HintNoReplacements(sentences[i], match);
-                            finalResult.addHint(hint);
-                            break;
-                        }   
-                    }
+                    //else if(checkSentenceTopicMatch(sentences[i], profileInferences, ident))
+                    //{
+                    //    SanitizationHint hint = new HintNoReplacements(sentences[i], 1);
+                    //    finalResult.addHint(hint);
+                    //}
                 }
             }
             
@@ -231,5 +264,78 @@ public class SanitizationSimple extends Sanitization
         }
         else
             return null;
+    }
+    
+    private boolean checkSentenceTopicMatch(Sentence sentence, List<Topic[]> profileInferences, TopicIdentifier ident)
+    {
+        // only operate on sentences we haven't confirmed to be privacy violations
+                
+        // get inference on full sentence
+         Topic[] topicList = ident.readFromStrings(new String[] {sentence.getText()});
+
+         String[] words = sentence.getText().split(" ");
+
+         // Start with a window size of 2 and work up to max window size, increment each time
+         int windowSize = MIN_WINDOW;
+         while(windowSize < MAX_WINDOW)
+         {
+             // iterate through words, taking <window size> words at a time.
+             int windowStart = 0;
+             while(windowStart <= words.length - windowSize)
+             {
+                 // create a new array containing words which are not inside the window
+
+                 int index = 0;
+                 String[] wordsMinusWindow = new String[words.length - windowSize];
+                 for(int wordIndex=0; wordIndex<words.length; wordIndex++)
+                 {
+                     if(index < wordsMinusWindow.length && (wordIndex < windowStart || wordIndex >= windowStart + windowSize))
+                     {
+                         wordsMinusWindow[index] = words[wordIndex];
+                         index++;
+                     }
+                 }
+
+                 // Run inference on sentence without window
+                 Topic[] topicListNoWindow = ident.readFromStrings(wordsMinusWindow);
+
+                 // Compare inferences to each inference based on profile items
+                 for(Topic[] inf : profileInferences)
+                {
+                    TopicMatcher matcherWithWindow = new TopicMatcher(inf, topicList);   
+                    TopicMatcher matcherWithoutWindow = new TopicMatcher(inf, topicListNoWindow); 
+                    double match = matcherWithWindow.getMatchValue();
+                    double matchNoWindow = matcherWithoutWindow.getMatchValue();
+
+                    if(match > EQUALITY_THRESHOLD && match > matchNoWindow)
+                    {
+                        return true;
+                       // break;
+                    }   
+                }
+
+                 windowStart++;
+             }
+
+             windowSize++;
+         }
+
+         return false;
+    }
+
+    private boolean checkSentenceNgramMatch(Sentence sentence, List<Map<String, Integer>> ngrams)
+    {
+        String text = sentence.getText().toUpperCase();
+        
+        for(Map<String, Integer> topicalNGram: ngrams)
+        {
+            for(String keyTerm: topicalNGram.keySet())
+            {
+                if(text.contains(keyTerm.toUpperCase()))
+                    return true;
+            }
+        }
+        
+        return false;
     }
 }
